bfba2d1b5d42fd56c866df3acf40308f431a0b37
Dakota Hester <admin@dakotahester.com>
2023-06-14 17:33:49 -0500

    use rasterio window util

--- example.py
+++ example.py
@@ -2,16 +2,47 @@ import geosampler
 import rasterio
 import os
 
+# class GridSampler:
+#     def __init__(self: object, 
+#                  input_dataset: rasterio.DatasetReader,
+#                  target_dataset: rasterio.DatasetReader,
+#                  patch_size: int,
+#                  split: float,
+#                  method: str,
+#                  return_remaining_dataset: bool):
+#         self.dataset = dataset
+#         self.patch_size = patch_size
+#         self.split = split
+#         self.method = method
+#         self.aois = []
+#         self.input_patches = []
+#         self.output_pathces = []
+                
+#     def _get_input_numpy_array(self):
+#         return self.dataset.read()
+    
+#     def _get_target_numpy_array(self):
+#         return self.dataset.read()
+    
+#     def _valid_aoi(self, aoi):
+#         masks = np.merge(self.input_dataset.read_
+    
+#     def _create_grid(self):
+#         input_array = self._get_input_numpy_array()
+
 # Open a raster dataset
-dataset_path = os.path.join('data', 'nyc_temp_NAIP_merged_aligned.tif')
-with rasterio.open(dataset_path) as dataset:
-    test_samples, test_dataset = geosampler.grid_sampler(
-        dataset=dataset,
-        patch_size=600,
-        split=0.2,
-        method='max_js_distance', # 'max_js_distance' or 'random'
-        return_remaining_dataset=True, # returns samples and dataset minmus samples
-    )
+input_dataset_path = os.path.join('data', 'nyc_temp_NAIP_merged_aligned.tif')
+target_dataset_path = os.path.join('data', 'nyc_LC_aligned.tif')
+with rasterio.open(input_dataset_path) as input_dataset:
+    with rasterio.open(target_dataset_path) as target_dataset:
+        test_samples, test_dataset = geosampler.grid_sampler(
+            input_dataset=input_dataset,
+            target_dataset=target_dataset,
+            patch_size=600,
+            split=0.2,
+            method='max_js_distance', # 'max_js_distance' or 'random'
+            return_remaining_dataset=True, # returns samples and dataset minmus samples
+        )
     # val_samples, train_dataset = geosampler.grid_sampler(
     #     dataset=test_dataset,
     #     patch_size=256,
--- geosampler/grid_sampler.py
+++ geosampler/grid_sampler.py
@@ -1,6 +1,7 @@
 import rasterio
 from rasterio.plot import show
 from rasterio.transform import Affine
+from rasterio.windows import Window
 import numpy as np
 import matplotlib.pyplot as plt
 from geosampler import grid_sampler
@@ -8,12 +9,13 @@ import os
 from scipy.spatial.distance import jensenshannon
 
 def grid_sampler(
-    dataset: rasterio.io.DatasetReader | list[rasterio.io.DatasetReader], 
+    input_dataset: rasterio.io.DatasetReader,
+    target_dataset: rasterio.io.DatasetReader,
     patch_size: int | tuple[int, int], 
     split: float, 
     method: str = 'max_js_distance', 
     return_remaining_dataset: bool = True
-) -> list[rasterio.io.DatasetReader] | tuple[list, rasterio.io.DatasetReader]:
+) -> tuple[list, rasterio.io.DatasetReader]:
     """Sample a raster dataset using a grid sampling method.
     
     Args:
@@ -26,29 +28,37 @@ def grid_sampler(
         list | tuple[list, rasterio.io.DatasetReader]: The samples and the remaining dataset (sans already sampled patches).
     """
     
-    if type(dataset) == rasterio.io.DatasetReader:
-        dataset = [dataset]
     
-    show_datasets = False
-    if show_datasets: 
-        for dataset in dataset:
-            fig, (axnir, axr, axg, axb) = plt.subplots(1, 4, figsize=(25, 10))
-            show(dataset.read(4), transform=dataset.transform, ax=axnir, title='NIR', cmap='viridis')
-            show(dataset.read(1), transform=dataset.transform, ax=axr, title='Red', cmap='Reds')
-            show(dataset.read(2), transform=dataset.transform, ax=axg, title='Green', cmap='Greens')
-            show(dataset.read(3), transform=dataset.transform, ax=axb, title='Blue', cmap='Blues')
-            plt.show()
+    # show_datasets = False
+    # if show_datasets: 
+    #     for dataset in dataset:
+    #         fig, (axnir, axr, axg, axb) = plt.subplots(1, 4, figsize=(25, 10))
+    #         show(dataset.read(4), transform=dataset.transform, ax=axnir, title='NIR', cmap='viridis')
+    #         show(dataset.read(1), transform=dataset.transform, ax=axr, title='Red', cmap='Reds')
+    #         show(dataset.read(2), transform=dataset.transform, ax=axg, title='Green', cmap='Greens')
+    #         show(dataset.read(3), transform=dataset.transform, ax=axb, title='Blue', cmap='Blues')
+    #         plt.show()
     
-    patches = get_patches(dataset, patch_size)
-    distances = get_distances(patches)
-    print(distances)
+    patches = get_patches(input_dataset, target_dataset, patch_size)
+    # distances = get_distances(patches)
+    # print(distances)
     
     return None, None
 
+def is_aoi_valid(
+    aoi: tuple[slice, slice],
+    input_dataset: rasterio.io.DatasetReader,
+    target_dataset: rasterio.io.DatasetReader
+) -> bool:
+    input_np, target_np = input_dataset.read_masks(window=Window.from_slices(aoi[0], aoi[1])), target_dataset.read_masks(window=Window.from_slices(aoi[0], aoi[1]))
+    if input_dataset.nodata in input_np or target_dataset.nodata in target_np: return False
+    else: return True
+
 def get_patches(
-    dataset: rasterio.io.DatasetReader, 
+    input_dataset: rasterio.io.DatasetReader, 
+    target_dataset: rasterio.io.DatasetReader,
     patch_size: int | tuple[int, int]
-) -> list[rasterio.io.DatasetReader]:
+) -> list[tuple[rasterio.io.DatasetReader, rasterio.io.DatasetReader]]:
     """Get a list of patches from a raster dataset.
     
     Args:
@@ -56,65 +66,56 @@ def get_patches(
         patch_size (int): The size of the patches to sample. E.g. 256 or (256, 512).
     Returns:
         list[rasterio.io.DatasetReader]: A list of patches.
+    Raises:
+        ValueError: If the input and target datasets have different dimensions.
     """
+    if input_dataset.width != target_dataset.width or input_dataset.height != target_dataset.height:
+        raise ValueError(f'Input and target datasets must have the same dimensions. \n Input dataset: {input_dataset.width}x{input_dataset.height} \n Target dataset: {target_dataset.width}x{target_dataset.height}')
     if type(patch_size) == int: patch_size = (patch_size, patch_size)
     
-    for dataset in dataset:
-        dataset_np = dataset.read()
-        
-        original_transform = dataset.transform
-        original_crs = dataset.crs
-        
-        patches = []
+    input_patch_out_path = os.path.join('data', 'patches', 'input')
+    target_patch_out_path = os.path.join('data', 'patches', 'target')
+    if not os.path.exists(input_patch_out_path): os.makedirs(input_patch_out_path)
+    if not os.path.exists(target_patch_out_path): os.makedirs(target_patch_out_path)
     
-        for i in range(dataset_np.shape[1] // patch_size[0]):
-            for j in range(dataset_np.shape[2] // patch_size[1]):
-                
-                # dataset[:, distance from top, distance from left]
-                patch_np = dataset_np[:, i*patch_size[0]:(i+1)*patch_size[0], j*patch_size[1]:(j+1)*patch_size[1]]
-                patch_transform = Affine(original_transform[0], original_transform[1], original_transform[2] + j*patch_size[1], # left
-                                        original_transform[3], original_transform[4], original_transform[5] - i*patch_size[0]) # top
-                
-                patch = rasterio.open(
-                    os.path.join('data', 'patches', f'patch_{i:05d}_{j:05d}.tif'),
-                    'w',
-                    driver='GTiff',
-                    height=patch_np.shape[1],
-                    width=patch_np.shape[2],
-                    count=patch_np.shape[0],
-                    dtype=patch_np.dtype,
-                    crs=original_crs,
-                    transform=patch_transform,
-                )
-                patches.append(patch)
-                # patch.write(patch_np)
-                
-                # fig, ax = plt.subplots(1, 2, figsize=(25, 10))
-                # show(dataset.read(1), transform=dataset.transform, ax=ax[0], title='Original')
-                # show(patch.read(1), transform=patch.transform, ax=ax[1], title='Patch')
-                # plt.show()
+    height = input_dataset.height
+    width = input_dataset.width
+    patches_arrays = []
+    
+    patches = []
+    
+    # get all valid patches
+    for i in range(height // patch_size[1]):
+        for j in range(width // patch_size[0]):
+            
+            # dataset[:, distance from top, distance from left]
+            aoi_slices = (slice(i*patch_size[0], (i+1)*patch_size[0]), slice(j*patch_size[1], (j+1)*patch_size[1]))
+            aoi = (i*patch_size[0], (i+1)*patch_size[0], j*patch_size[1], (j+1)*patch_size[1])
+            if not is_aoi_valid(aoi_slices, input_dataset, target_dataset): # invalid patch    
+                continue
+            
+            patches_arrays.append((aoi, input_dataset.read(window=Window.from_slices(aoi_slices[0], aoi_slices[1])))) # target dataset should align with input dataset, so aoi is all that is needed
+    
+    # get js distance matrix
+    # for aoi, patch in zip(patches_arrays):
+    distance_matrix = np.zeros((len(patches_arrays), len(patches_arrays)))
+        
+    for i in range(len(patches_arrays)):
+        for j in range(len(patches_arrays)):
+            # matrix is symmetric
+            if i < j:
+                distance_matrix[j, i] = distance_matrix[i, j]
+            # don't compare patch to itself
+            elif i == j:
+                continue
+            # compute js distance
+            else:
+                for band in range(patches_arrays[0][1].shape[0]):
+                    histogram_ik, _ = np.histogram(patches_arrays[i][1][band, :, :], bins=256, range=(0, 255))
+                    histogram_jk, _ = np.histogram(patches_arrays[j][1][band, :, :], bins=256, range=(0, 255))
+                    # print(histogram_ik.shape, histogram_jk.shape)
+                    distance_matrix[i, j] += jensenshannon(histogram_ik, histogram_jk)
+    
+    print(distance_matrix)
         
     return patches
-
-def get_distances(patches: list[rasterio.io.DatasetReader]) -> np.ndarray:
-    """Get the js distances between all patches.
-    
-    Args:
-        patches (list[rasterio.io.DatasetReader]): A list of patches.
-    Returns:
-        np.ndarray: A matrix of distances between all patches.
-    """
-    
-    distances = np.zeros((len(patches), len(patches)))
-    for i in range(len(patches)):
-        for j in range(len(patches)):
-            if patches[i] == patches[j]: continue
-            elif j > i: 
-                distances[i, j] = distances[j, i]
-                continue
-            else:
-                for band in range(patches[i].shape[2]):
-                    histogram_ik = np.histogram(patches[i][:, :, band], bins=256, range=(0, 255))
-                    histogram_jk = np.histogram(patches[j][:, :, band], bins=256, range=(0, 255))
-                    print(histogram_ik, histogram_jk)
-                    distances[i, j] += jensenshannon(histogram_ik, histogram_jk)
